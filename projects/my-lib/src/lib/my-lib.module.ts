fa8c5c7617b16d92ce65200187c49327db41a6df
Franic <franpr90@gmail.com>
2019-11-13 18:28:53 +0000

    created lib added jest

---
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ButtonComponent } from './components/button/button.component';
+
+
+@NgModule({
+  declarations: [ButtonComponent],
+  imports: [
+  ],
+  exports: [ButtonComponent]
+})
+export class MyLibModule { }
